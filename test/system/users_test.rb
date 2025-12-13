6ae4bde7b526ea838acfa0157e14e11be7c6822c
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-12-13 22:58:14 +0700

    Can forget password

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -20,4 +20,16 @@ class UsersTest < ApplicationSystemTestCase
 
     assert_text "Sign Out"
   end
+
+  test "user can request password reset" do
+    # Create a user with FactoryBot
+    user = create(:user, email: "testuser@example.com", password: "Banana#13")
+    visit new_user_password_path
+
+    fill_in "user_email", with: user.email
+
+    click_button "commit"
+    assert_text "You will receive an email with instructions on how to reset your password in a few minutes."
+    assert_current_path new_user_session_path
+  end
 end
