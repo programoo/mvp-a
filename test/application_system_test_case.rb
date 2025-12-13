c5eefc4b3a3d66da92baf9056adaae7a8fbaaa4e
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-12-13 22:50:29 +0700

    Can run sign out

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include FactoryBot::Syntax::Methods
 
-  # driven_by :selenium, using: :headless_chrome
-  driven_by :selenium, using: :chrome, screen_size: [1400, 900]
+  driven_by :selenium, using: :headless_chrome
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 900]
 
   def sign_in(user)
     visit new_user_session_path
